94d087242f13e5b9126a2ddbfb0f6c75534a0ef3
samuelbarin <samuel.baron.pl@gmail.com>
2021-01-03 17:28:34 +0100

    Added a skeleton content in account.vue

--- pages/account.vue
+++ pages/account.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
     <app-page-name path="Account"></app-page-name>
+    <div class="d-flex justify-center" style=" position: absolute; top: 20%; flex-direction: column; width: 70%; margin-left: 15%">
+      <span class="blue--text">Login:</span>
+      <span>samuelbaron</span>
+      <br>
+      <span class="blue--text">Email:</span>
+      <span>samuel.baron.pl@gmail.com</span>
+    </div>
     <div class="d-flex justify-center">
+      <v-btn
+        style="position: absolute; bottom: 31%"
+        color="primary"
+        dark
+        width="80%"
+      >
+        Change Login
+      </v-btn>
+      <v-btn
+        style="position: absolute; bottom: 22%"
+        color="primary"
+        dark
+        width="80%"
+      >
+        Change Email
+      </v-btn>
+      <v-btn
+        style="position: absolute; bottom: 13%"
+        color="primary"
+        dark
+        width="80%"
+      >
+        Change Password
+      </v-btn>
       <v-btn
         @click="logout"
         style="position: absolute; bottom: 4%"
-        rounded
         color="red"
         dark
         width="80%"
